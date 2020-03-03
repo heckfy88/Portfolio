a34b54fe238b8ebf2f9def2de63a1560cd09179c
heckfy88 <54860448+heckfy88@users.noreply.github.com>
2020-03-04 01:34:29 +0300

    Update Index_view.php

--- View/Index_view.php
+++ View/Index_view.php
@@ -1,7 +1,7 @@
 <?php
 include "Registration_form.html";
 //  Controller: assigns user data to $user array
-include "/Users/ruslanagaev/Desktop/PHP_STORM/Portfolio/Controller/Registration_form_controller.php";
+include "Portfolio/Controller/Registration_form_controller.php";
 //  Model: sends user data to DB.
-include "/Users/ruslanagaev/Desktop/PHP_STORM/Portfolio/Model/Registration_form_model.php";
+include "Portfolio/Model/Registration_form_model.php";
 insert_into_db($user); // Function from model
